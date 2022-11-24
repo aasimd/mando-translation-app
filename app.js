fdd21964b94c34efd5149acd5bc3e00dc8ce7868
aasimd <dongarkar21@gmail.com>
2022-11-24 18:09:51 +0530

    done

---
+++ app.js
@@ -0,0 +1,33 @@
+var inputText = document.querySelector("#txt-area");
+var translateButton = document.querySelector(".translate-button");
+var outputBox = document.querySelector(".outputbox");
+var url = "https://api.funtranslations.com/translate/mandalorian.json"
+function errorHandler(error){
+    console.log("error occured ", error);
+    alert('there was an error, try again after sometime.')
+}
+function constructUrl(text){
+        return url + "?text=" + text 
+    }
+function clickHandler(){
+    var text = inputText.value; 
+    
+    fetch(constructUrl(text))
+    .then (response => response.json() )
+    .then (json => {
+        var translatedText = json.contents.translated;
+        outputBox.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+
+
+
+
+
+
+
+
+
+translateButton.addEventListener("click", clickHandler);
+
